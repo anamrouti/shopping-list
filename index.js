28f13a005d74059c9eaa383c884753e64681ff73
anamrouti <57967377+anamrouti@users.noreply.github.com>
2020-03-26 01:30:29 -0400

    worked on checked button

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ $(function handleShoppingList(){
   $('#js-shopping-list-form').submit(event =>{
     event.preventDefault();
     const myItem = $('.js-shopping-list-entry').val();
+    $('#shopping-list-entry').val('');
     $('.shopping-list').append(
     `<li><span class="shopping-item">${myItem}</span>
         <div class="shopping-item-controls">
@@ -14,8 +15,8 @@ $(function handleShoppingList(){
         </div>
       </li>`)
    });
-   $('.shopping-list').on('click', '.shopping-item-toggle', function(event) {
-    $(this).closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
+   $('#shopping-list').on('click', '.shopping-item-toggle', function(event) {
+    $(this).closest('li').find('.shopping-item').toggleClass('.shopping-item__checked');
   });
   $('.shopping-list').on('click', '.shopping-item-delete', function(event) {
     $(this).closest('li').remove();
